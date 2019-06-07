e85af8025416e0cdf731eef42e8fcecd186cc625
Apple <apple@Apples-MacBook-Pro-6.local>
2019-06-07 16:06:56 -0700

    SecondCommit

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let emojis = ["😊": "happy", "😔": "sad"]
+    let customMessages = ["happy" : "That's great! Always try to keep a positive attitude, it's more fun", "sad" : "Don't worry, it will get better. Try to stay positive and think of this as a lesson, not a failure"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func HappyEmoji(_ sender: UIButton) {
+        customMessages["Happy"]
+    }
+    @IBAction func SadEmoji(_ sender: UIButton) {
+        customMessages["Sad"]
     }
 
-
+    @IBAction func showMessage(sender: UIButton){
+        
+        let selectedEmotion = sender.titleLabel?.text
+        let message = emojis[selectedEmotion!]!
+        let realMessage = customMessages[message]!
+        let alertController = UIAlertController(title: realMessage, message: "", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+}
+    
+    
+    
+    
+    
+    
 }
 
